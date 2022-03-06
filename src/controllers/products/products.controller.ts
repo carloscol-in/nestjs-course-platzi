50ef03d8d4ef00c923217e4cd90985c43d388942
Carlos Colin <carloscolinn@outlook.es>
2022-03-06 00:50:35 -0600

    Added controllers

---
+++ src/controllers/products/products.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
+import { ParseIntPipe } from 'src/common/parse-int.pipe';
+import { Product } from 'src/entities/product.entity';
+import { ProductsService } from 'src/services/products/products.service';
+import { CreateProductDto } from 'src/dtos/products.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+  @Get()
+  getAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get('/:id')
+  getSingle(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateProductDto) {
+    return this.productsService.create(payload);
+  }
+
+  @Put(':id')
+  update(@Body() payload: object, @Param('id', ParseIntPipe) id: number) {
+    return this.productsService.update(id, payload);
+  }
+
+  @Delete(':id')
+  destroy(@Param('id', ParseIntPipe) id: number) {
+    return {
+      id,
+    };
+  }
+}
