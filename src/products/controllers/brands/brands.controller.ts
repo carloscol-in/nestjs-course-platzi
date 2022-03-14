aad5c3ef62c55e9a398777565a11f4018c04da37
Carlos C <contact@carloscolin.me>
2022-03-13 22:41:45 -0600

    Brand controller

--- src/products/controllers/brands/brands.controller.ts
+++ src/products/controllers/brands/brands.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('brands')
-export class BrandsController {}
+import {
+    Controller,
+    Get,
+    Param,
+    Post,
+    Body,
+    Put,
+    Delete,
+  } from '@nestjs/common';
+  import { ApiTags } from '@nestjs/swagger';
+  
+  import { BrandsService } from 'src/products/services/brands/brands.service';
+  import { CreateBrandDto, UpdateBrandDto } from 'src/products/dtos/brand.dto';
+  
+  @ApiTags('brands')
+  @Controller('brands')
+  export class BrandsController {
+    constructor(private brandsService: BrandsService) {}
+  
+    @Get()
+    findAll() {
+      return this.brandsService.findAll();
+    }
+  
+    @Get(':id')
+    get(@Param('id') id: string) {
+      return this.brandsService.findOne(id);
+    }
+  
+    @Post()
+    create(@Body() payload: CreateBrandDto) {
+      return this.brandsService.create(payload);
+    }
+  
+    @Put(':id')
+    update(@Param('id') id: string, @Body() payload: UpdateBrandDto) {
+      return this.brandsService.update(id, payload);
+    }
+  
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+      return this.brandsService.remove(id);
+    }
+  }
