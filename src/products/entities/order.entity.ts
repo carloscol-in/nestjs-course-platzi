be4c1e9d261a7244de7ab0797031637d4f069644
Carlos C <contact@carloscolin.me>
2022-03-14 20:57:57 -0600

    Order entity

--- src/products/entities/order.entity.ts
+++
@@ -1,8 +0,0 @@
-import { User } from 'src/users/entities/user.entity';
-import { Product } from 'src/products/entities/product.entity';
-
-export class Order {
-  date: Date;
-  user: User;
-  products: Product[];
-}
