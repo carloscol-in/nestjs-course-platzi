3276ecab879392d10107eb5bc1ac8d6a2bc3b20a
Carlos Colin <carloscolinn@outlook.es>
2022-03-23 23:25:21 -0600

    Add postgres config

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,14 @@ export default registerAs('config', () => {
       name: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
     },
-    apiKey: process.env.API_KEY,
+    postgres: {
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST,
+    },
     port: process.env.PORT,
+    apiKey: process.env.API_KEY,
   };
 });
