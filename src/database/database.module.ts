f4dbb3b6965478c48b895e696352f5e3ac7e4f97
Carlos Colin <carloscolinn@outlook.es>
2022-03-23 23:25:42 -0600

    Add database module config with typeorm

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,13 +1,45 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import config from 'src/config';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { username, password, host, database, port } =
+          configService.postgres;
+        return {
+          type: 'postgres',
+          username,
+          password,
+          database,
+          host,
+          port,
+        };
+      },
+      inject: [config.KEY],
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
       useValue: 'hello',
     },
+    // {
+    //   provide: 'PG',
+    //   useFactory: (configService: ConfigType<typeof config>) => {
+    //     const client = new Client(configService.postgres);
+
+    //     client.connect();
+
+    //     return client;
+    //   },
+    //   inject: [config.KEY],
+    // },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', TypeOrmModule],
 })
 export class DatabaseModule {}
