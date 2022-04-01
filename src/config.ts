49a630c3d9ad8f766f5d9f6332d220315ea5ed8d
Carlos Colin <carloscolinn@outlook.es>
2022-03-31 23:48:08 -0600

    Add config for mysql

--- src/config.ts
+++ src/config.ts
@@ -13,6 +13,13 @@ export default registerAs('config', () => {
       password: process.env.POSTGRES_PASSWORD,
       host: process.env.POSTGRES_HOST,
     },
+    mysql: {
+      port: parseInt(process.env.MYSQL_PORT, 10),
+      database: process.env.MYSQL_DB,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.MYSQL_HOST,
+    },
     port: process.env.PORT,
     apiKey: process.env.API_KEY,
   };
