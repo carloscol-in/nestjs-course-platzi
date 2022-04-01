f09d35b14b59a07f7a4ce7ee227e93fe8e93a4db
Carlos Colin <carloscolinn@outlook.es>
2022-04-01 00:30:40 -0600

    Add ApiProperty decorator

--- src/products/dtos/product.dto.ts
+++ src/products/dtos/product.dto.ts
@@ -1,3 +1,5 @@
+import { PartialType, ApiProperty } from '@nestjs/swagger';
+
 import {
   IsString,
   IsNumber,
@@ -5,30 +7,38 @@ import {
   IsNotEmpty,
   IsPositive,
 } from 'class-validator';
-import { PartialType } from '@nestjs/swagger';
+import { ICreateDto } from 'src/common/interfaces/dtos/create.interface';
+import { IUpdateDto } from 'src/common/interfaces/dtos/update.interface';
 
-export class CreateProductDto {
+export class CreateProductDto implements ICreateDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly description: string;
 
   @IsNumber()
   @IsPositive()
   @IsNotEmpty()
+  @ApiProperty()
   readonly price: number;
 
   @IsNumber()
   @IsPositive()
   @IsNotEmpty()
+  @ApiProperty()
   readonly stock: number;
 
   @IsUrl()
   @IsNotEmpty()
+  @ApiProperty()
   readonly image: string;
 }
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto
+  extends PartialType(CreateProductDto)
+  implements IUpdateDto {}
